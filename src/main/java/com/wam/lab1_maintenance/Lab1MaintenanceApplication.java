0762ddd4c58ae51ab233ff6ff8fbf93a700e7c75
NWilliRex <panda16520@gmail.com>
2025-09-11 18:35:59 -0400

    init lab1

---
+++ src/main/java/com/wam/lab1_maintenance/Lab1MaintenanceApplication.java
@@ -0,0 +1,15 @@
+package com.wam.lab1_maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1MaintenanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1MaintenanceApplication.class, args);
+    }
+
+    public static class Service {
+    }
+}
---
+++ src/test/java/com/wam/lab1_maintenance/Lab1MaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wam.lab1_maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1MaintenanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
