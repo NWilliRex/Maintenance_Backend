67f715eb2020183b8bdce61bf8a62ae88c4b1df5
2391650 <2391650@collegemv.qc.ca>
2025-10-16 17:29:41 -0400

    ajout corrections

--- src/test/java/com/wam/lab1_maintenance/Lab1MaintenanceApplicationTests.java
+++ src/test/java/com/wam/lab1_maintenance/Lab1MaintenanceApplicationTests.java
@@ -2,12 +2,17 @@ package com.wam.lab1_maintenance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Lab1MaintenanceApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(true, true);
     }
 
 }
